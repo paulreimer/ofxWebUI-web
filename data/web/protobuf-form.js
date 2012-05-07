82e27d236c87ee04d06163c87b6551a56a1477e0
Paul Reimer <paulreimer@gmail.com>
2012-05-07 12:36:21 -0700

    Latest javascript UI generation libs

--- data/web/protobuf-form.js
+++ data/web/protobuf-form.js
@@ -1,4 +1,4 @@
-$(document).bind("mobileinit", function(){
+$(document).bind("mobileinit", function() {
   var form = $('#form');
   var ui = new protobuf.ui;
   for (var field in ui.properties_)
@@ -7,6 +7,18 @@ $(document).bind("mobileinit", function(){
     var input = [];
     switch (options.widget)
     {
+      case 'string':
+        input.push(
+          '<div data-role="fieldcontain">',
+            '<label for="', field, '">', field, ':</label>',
+            '<input type="text" ',
+              'name="', field, '" ',
+              'id="', field, '" ',
+              'value="" />',
+          '</div>'
+        );
+        break;
+
       case 'slider':
         var step = options['step'] || "1";
         input.push(
@@ -22,6 +34,7 @@ $(document).bind("mobileinit", function(){
           '</div>'
         );
         break;
+
       case 'radio':
         input.push(
           '<div data-role="fieldcontain">',
@@ -45,6 +58,33 @@ $(document).bind("mobileinit", function(){
         );
         break;
 
+      case 'image-list':
+        input.push(
+          '<div data-role="fieldcontain">',
+            '<fieldset data-role="controlgroup" id="', field, '">',
+              '<legend>', field, '</legend>',
+              '<ul data-role="listview">'
+        );
+        for (var idx in options.choices)
+        {
+          var item = options.choices[idx];
+          var name = [field, idx].join('_');
+          input.push(
+            '<li><a href="#">',
+              '<img src="', item.image, '" />',
+              '<h3>', item.title, '</h3>',
+              '<p>', item.title, '</p>',
+            '</a></li>'
+          );
+        }
+
+        input.push(
+            '</ul>',
+            '</fieldset>',
+          '</div>'
+        );
+        break;
+
       case 'toggle':
         input.push(
           '<div data-role="fieldcontain">',
@@ -55,6 +95,19 @@ $(document).bind("mobileinit", function(){
               '<option value="0">Off</option>',
               '<option value="1">On</option>',
             '</select>',
+          '</div>'
+        );
+        break;
+
+      case 'color':
+        input.push(
+          '<div data-role="fieldcontain">',
+            '<label for="', field, '">', field, '</label>',
+            '<input type="text" ',
+              'name="', field, '" ',
+              'id="', field, '"',
+              ' value="#000000" />',
+            '<div class="colorpicker"></div>',
           '</div>'
         );
         break;
@@ -70,15 +123,17 @@ $(document).bind("mobileinit", function(){
           return;
 
         var ui = new protobuf.ui;
-        var serialized = ws.binaryType? new PROTO.ByteArrayStream
-          : new PROTO.Base64Stream;
+        var serialized = (ws.binaryType==='arraybuffer')?
+          new PROTO.ByteArrayStream :
+          new PROTO.Base64Stream;
         var value = null;
 
         switch (evt.data['widget'])
         {
-          case 'slider':
-            value = $('input', this).val();
+          case 'color':
+            value = parseInt($('input', this).val().replace('#', '0x'));
             break;
+
           case 'radio':
             $('input[type="radio"]', this).each(function(idx, radio) {
               var checked = ($(this).attr('id') === evt.target.id);
@@ -93,6 +148,41 @@ $(document).bind("mobileinit", function(){
           case 'toggle':
             value = ( $('select', this)[0].selectedIndex > 0 );
             break;
+
+          case 'string':
+          case 'slider':
+            value = $('input', this).val();
+            break;
+
+          default:
+            value = this.value;
+            break;
+        }
+
+        ui[evt.data['field']] = value;
+        ui.SerializeToStream(serialized);
+
+        ws.send(serialized.getString());
+      });
+
+      $('li', el).click({
+        'field': field,
+        'widget': options.widget
+      }, function(evt) {
+        if (!live || !ws)
+          return;
+
+        var ui = new protobuf.ui;
+        var serialized = (ws.binaryType==='arraybuffer')?
+          new PROTO.ByteArrayStream :
+          new PROTO.Base64Stream;
+        var value = null;
+
+        switch (evt.data['widget'])
+        {
+          case 'image-list':
+            value = $(this).index();
+            break;
         }
 
         ui[evt.data['field']] = value;
@@ -103,4 +193,11 @@ $(document).bind("mobileinit", function(){
       form.append(el);
     }
   }
+
+  $('.colorpicker').each(function() {
+    $(this).farbtastic({
+      callback: $(this).prev('input'),
+      width: 200
+    });
+  });
 });
---
+++ data/web/protobuf-init.js
@@ -0,0 +1,156 @@
+function get_appropriate_ws_url(port)
+{
+	var pcol;
+	var u = document.URL;
+
+	/*
+	 * We open the websocket encrypted if this page came on an
+	 * https:// url itself, otherwise unencrypted
+	 */
+
+	if (u.substring(0, 5) == "https") {
+		pcol = "wss://";
+		u = u.substr(8);
+	} else {
+		pcol = "ws://";
+		if (u.substring(0, 4) == "http")
+			u = u.substr(7);
+	}
+
+	u = u.split('/');
+
+  var url;
+  if (port !== undefined)
+  {
+    var socket = u[0].split(':');
+    url = socket[0] + ":" + port;
+  }
+  else {
+   url = u[0];
+  }
+
+	return pcol + url;
+}
+
+$(document).ready(function() {
+  var ws_url_el = $("#ws_url");
+  var title_sep = ' | ';
+  /*
+  var title_base = null;
+  if (document.title.indexOf(title_sep)>=0)
+    title_base = document.title.split(title_sep)[0]
+  */
+  var title_base = "G++ Sign";
+
+  var disconnect = function() {
+    try {
+      if (ws.readyState !== ws.CLOSING && ws.readyState !== ws.CLOSED)
+      {
+        ws.close(0);
+      }
+    } catch(exception) {
+      console.log('Error' + exception);  
+    }
+  };
+  var connect = function() {
+    var ws_url = get_appropriate_ws_url(7681);
+    try {
+      if (window.MozWebSocket)
+        ws = new MozWebSocket(ws_url, "ofx");
+      else if (window.WebSocket)
+        ws = new WebSocket(ws_url, "ofx");
+      else
+        return;
+
+      /*
+      if (ws.binaryType)
+      {
+        ws.binaryType = 'arraybuffer';
+      }
+      */
+
+      ws.onopen = function() {
+        ws_url_el
+          .attr('data-theme', 'd')
+          .click(disconnect);
+        $('.ui-icon', ws_url_el)
+          .addClass('ui-icon-check')
+          .removeClass('ui-icon-refresh')
+        $('.ui-btn-text', ws_url_el).text(ws_url);
+        $('#ws_status').text('Connected');
+        document.title = title_base + title_sep + 'Connected';
+      }
+
+      ws.onmessage = function(msg) {
+        var ui = new protobuf.ui;
+        var serialized = (ws.binaryType==='arraybuffer')?
+          new PROTO.ByteArrayStream(msg.data) :
+          new PROTO.Base64Stream(msg.data);
+
+        ui.ParseFromStream(serialized);
+
+        live = false;
+        for (var field in ui.values_)
+        {
+          var el = $('#'+field);
+          var options = ui.properties_[field].options;
+          switch (options.widget)
+          {
+            case 'color':
+              var color = '#'+ui.values_[field].toString(16);
+              $.farbtastic(el.next('.colorpicker')).setColor(color);
+              break;
+            case 'toggle':
+              el[0].selectedIndex = ui.values_[field]? 1:0;
+              el.slider("refresh");
+              break;
+            case 'radio':
+              var radios = $('input[type="radio"]', el);
+              radios.attr('checked', false);
+
+              var name = [field, ui.values_[field]].join('_');
+              $('input[type="radio"]#'+name).attr('checked', true);
+
+              radios.checkboxradio('refresh');
+              break;
+
+            case 'slider':
+              el.val(ui.values_[field]).slider("refresh");
+              break;
+
+            case 'string':
+              el.val(ui.values_[field]);
+              break;
+
+            case 'image-list':
+              console.log("implement me, too!");
+              break;
+
+            default:
+              el.value = ui.values_[field];
+              break;
+          }
+        }
+        live = true;
+      } 
+
+      ws.onclose = function(){
+        ws_url_el
+          .attr('data-theme', 'b')
+          .click(connect);
+        $('.ui-icon', ws_url_el)
+          .addClass('ui-icon-refresh')
+          .removeClass('ui-icon-check')
+        $('.ui-btn-text', ws_url_el).text("Reconnect");
+        $('#ws_status').text('Disconnected');
+        document.title = title_base + title_sep + 'Disconnected';
+      }
+    } catch(exception) {
+      console.log('Error' + exception);  
+    }
+  };
+
+  connect();
+  ws_url_el.click(connect);
+});
+
