7dea5db30e5836005f55fb2937932e96e0e9de1c
Paul Reimer <paulreimer@gmail.com>
2012-06-10 16:52:24 -0700

    Moved farbtastic to lib/ directory, removed trailing comma on json list for dumb parsers

--- min.sh
+++ min.sh
@@ -3,7 +3,7 @@
 : > ui-deps.min.js
 yuicompressor swfobject.js    >> ui-deps.min.js
 yuicompressor web_socket.js   >> ui-deps.min.js
-yuicompressor farbtastic.js   >> ui-deps.min.js
+yuicompressor lib/farbtastic.js >> ui-deps.min.js
 yuicompressor protobuf.js     >> ui-deps.min.js
 
 : > ui.min.js
