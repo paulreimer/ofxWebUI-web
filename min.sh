b263e89d59a3093089c940abc02bb144bf4236b5
Paul Reimer <paulreimer@gmail.com>
2012-06-14 20:59:50 -0700

    Added ui-multitouch.js to min.sh

--- min.sh
+++ min.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 : > ui-deps.min.js
-yuicompressor swfobject.js    >> ui-deps.min.js
-yuicompressor web_socket.js   >> ui-deps.min.js
+yuicompressor swfobject.js      >> ui-deps.min.js
+yuicompressor web_socket.js     >> ui-deps.min.js
 yuicompressor lib/farbtastic.js >> ui-deps.min.js
-yuicompressor protobuf.js     >> ui-deps.min.js
+yuicompressor protobuf.js       >> ui-deps.min.js
 
 : > ui.min.js
-yuicompressor ui-templates.js >> ui.min.js
-yuicompressor ui-sync.js      >> ui.min.js
-yuicompressor ui-form.js      >> ui.min.js
-yuicompressor ui-init.js      >> ui.min.js
+yuicompressor ui-templates.js   >> ui.min.js
+yuicompressor ui-sync.js        >> ui.min.js
+yuicompressor ui-form.js        >> ui.min.js
+yuicompressor ui-init.js        >> ui.min.js
+yuicompressor ui-multitouch.js  >> ui.min.js
+
