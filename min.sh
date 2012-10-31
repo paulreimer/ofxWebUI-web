6e3480995c7bf3c2f83a9a8298942b2bf9fd7cf8
Paul Reimer <paul@p-rimes.net>
2012-10-30 19:51:03 -0700

    Upgraded jquery mobile from 1.1.1 to 1.2.0, switched to .min.gz.js manually gzipped javascripts for CDN use

--- min.sh
+++ min.sh
@@ -7,6 +7,8 @@ yuicompressor lib/web_socket.js >> ui-deps.min.js
 yuicompressor lib/farbtastic.js >> ui-deps.min.js
 yuicompressor lib/protobuf.js   >> ui-deps.min.js
 
+gzip -9 -c ui-deps.min.js > ui-deps.min.gz.js
+
 : > ui.min.js
 yuicompressor ui-templates.js   >> ui.min.js
 yuicompressor ui-sync.js        >> ui.min.js
@@ -14,3 +16,4 @@ yuicompressor ui-form.js        >> ui.min.js
 yuicompressor ui-init.js        >> ui.min.js
 #yuicompressor ui-multitouch.js  >> ui.min.js
 
+gzip -9 -c ui.min.js > ui.min.gz.js
